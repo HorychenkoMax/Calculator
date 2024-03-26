bb738a42a3efab85b072704768f87ebab96ea148
Horychenko Maksym <horychenkomax@gmail.com>
2024-03-26 12:51:38 +0200

    Modified MainActivity and added logic for numbers

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.calculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView mainText;
+    private TextView supportingText;
+    private Button button_0;
+    private Button button_1;
+    private Button button_2;
+    private Button button_3;
+    private Button button_4;
+    private Button button_5;
+    private Button button_6;
+    private Button button_7;
+    private Button button_8;
+    private Button button_9;
+    String mainString = "";
+    String supportingString = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +39,54 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mainText = findViewById(R.id.main_text);
+        supportingText = findViewById(R.id.supporting_text);
+        button_0 = (Button) findViewById(R.id.button_0);
+        button_1 = (Button) findViewById(R.id.button_1);
+        button_2 = (Button) findViewById(R.id.button_2);
+        button_3 = (Button) findViewById(R.id.button_3);
+        button_4 = (Button) findViewById(R.id.button_4);
+        button_5 = (Button) findViewById(R.id.button_5);
+        button_6 = (Button) findViewById(R.id.button_6);
+        button_7 = (Button) findViewById(R.id.button_7);
+        button_8 = (Button) findViewById(R.id.button_8);
+        button_9 = (Button) findViewById(R.id.button_9);
+    }
+
+
+    public void buttonNumber(View v){
+        int viewId = v.getId();
+        if(viewId == R.id.button_0){
+            mainString += "0";
+            mainText.setText(mainString);
+        } else if (viewId == R.id.button_1) {
+            mainString += "1";
+            mainText.setText(mainString);
+        } else if (viewId == R.id.button_2) {
+            mainString += "2";
+            mainText.setText(mainString);
+        }else if (viewId == R.id.button_3) {
+            mainString += "3";
+            mainText.setText(mainString);
+        }else if (viewId == R.id.button_4) {
+            mainString += "4";
+            mainText.setText(mainString);
+        }else if (viewId == R.id.button_5) {
+            mainString += "5";
+            mainText.setText(mainString);
+        }else if (viewId == R.id.button_6) {
+            mainString += "6";
+            mainText.setText(mainString);
+        }else if (viewId == R.id.button_7) {
+            mainString += "7";
+            mainText.setText(mainString);
+        }else if (viewId == R.id.button_8) {
+            mainString += "8";
+            mainText.setText(mainString);
+        }else if (viewId == R.id.button_9) {
+            mainString += "9";
+            mainText.setText(mainString);
+        }
     }
 }
